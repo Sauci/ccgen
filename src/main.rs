94e78ca424082222642b1d745953592578970257
wheelin <greg.emry@gmail.com>
2020-10-14 18:53:58 +0200

    small modifications of main.rs, no functional changes

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@
 
 extern crate panic_halt;
 
-use cortex_m_rt::entry;
-
-use stm32f1::stm32f103::interrupt;
-
 mod crkcam;
 mod hwsiggen;
 mod system;
 mod periph;
+mod com;
 
-use crkcam::{cam::*, cam_cfg::*, crk::*, crk_cfg::*};
+use cortex_m_rt::entry;
+use stm32f1::stm32f103::interrupt;
+use cortex_m::asm;
+
+use crkcam::{
+    cam::*, 
+    cam_cfg::*, 
+    crk::*, 
+    crk_cfg::*
+};
 use crkcam::siggen::CrkCamSigGen;
 use hwsiggen::Timer;
 
@@ -27,16 +33,17 @@ fn TIM2() {
 #[entry]
 fn main() -> ! {
     system::init_clks();
-    
-    {
-        let tim = unsafe { &mut GEN_TIM };
-        let crk_gen: CrkSigGen = CrkSigGen::new(&CRK_CONFIGS[0]);
-        let cam_gen: CamSigGen = CamSigGen::new(&CAM_CONFIGS[0]);
-        
-        tim.initialize(cam_gen, crk_gen);
-        tim.set_speed_rpm(3000);
-        tim.start();
-    }
 
-    loop {}
+    let mut speed = 1000;
+    let mut cam_cfg_id = 0;
+    let mut crk_cfg_id = 0;
+    
+    let tim = unsafe { &mut GEN_TIM };
+    let crk_gen: CrkSigGen = CrkSigGen::new(&CRK_CONFIGS[crk_cfg_id]);
+    let cam_gen: CamSigGen = CamSigGen::new(&CAM_CONFIGS[cam_cfg_id]);
+    tim.initialize(cam_gen, crk_gen);
+    tim.set_speed_rpm(speed);
+    tim.start();
+    
+    loop { }
 }
